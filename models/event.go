73ed861f883fee720910e41fd0b6442ac46ce31f
thales-pinner <thales.neves@pinpeople.com.br>
2024-09-27 20:52:34 -0300

    create event model struct with gorm options

---
+++ models/event.go
@@ -0,0 +1,12 @@
+package event
+
+import "time"
+
+type Event struct {
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	NAME        string    `json:"name" gorm:"size:255;not null"`
+	Description string    `json:"description" gorm:"size:255;not null"`
+	Location    string    `json:"location" gorm:"default: 'planet earth'"`
+	DateTime    time.Time `json:"date_time"`
+	UserID      uint      `json:"user_id" gorm:"foreignKey:UserID"`
+}
