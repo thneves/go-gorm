316ba0d4848f801504f1e8abfd947b9a43114e68
thales-pinner <thales.neves@pinpeople.com.br>
2024-09-27 20:08:57 -0300

    add database package

---
+++ database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Password string
+	User     string
+	DBName   string
+	SSLMode  string
+}
+
+func NewConnection(config *Config) (*gorm.DB, error) {
+	// dsn means Data Source Name, it's a string to specify the information requiired to connect to a database
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,32 +1,43 @@
 package main
 
 import (
+	"go-gorm/database"
 	"log"
+	"os"
 	"time"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// dsn means Data Source Name, it's a string to specify the information requiired to connect to a database
-	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+	err := godotenv.Load(".env")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pgDB, err := db.DB()
+	config := &database.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASSWORD"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+
+	db_conn, err := database.NewConnection(config)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pgDB.SetConnMaxIdleTime(10)
-	pgDB.SetMaxOpenConns(100)
-	pgDB.SetConnMaxLifetime(time.Hour)
+	DB, err := db_conn.DB()
+
+	DB.SetConnMaxIdleTime(10)
+	DB.SetMaxOpenConns(100)
+	DB.SetConnMaxLifetime(time.Hour)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
