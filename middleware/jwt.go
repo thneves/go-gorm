0172265f04c482c5eea65cf44a15247ef366507c
thales-pinner <thales.neves@pinpeople.com.br>
2024-10-01 09:12:55 -0300

    add middleware for auth

---
+++ middleware/jwt.go
@@ -0,0 +1,60 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("a big mistery")
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// Get the token from Authorization Header
+		authHeader := context.GetHeader("Authorization")
+
+		if authHeader == "" {
+			context.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Authoriztion header is empty",
+			})
+			context.Abort()
+			return
+		}
+
+		// Split the token from the Bearer scheme
+		tokenString := strings.Split(authHeader, " ")[1]
+
+		// Parsed token
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, gin.Error{}
+			}
+
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			context.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid token",
+			})
+			context.Abort()
+			return
+		}
+
+		// Extract user_id from token clainms
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			context.Set("user_id", claims["user_id"])
+		} else {
+			context.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token claims",
+			})
+			return
+		}
+
+		context.Next()
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,32 +2,38 @@ package routes
 
 import (
 	"go-gorm/controllers"
+	"go-gorm/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func RegisterRoutes(server *gin.Engine, db *gorm.DB) {
-	server.GET("/events", func(c *gin.Context) {
-		controllers.GetEvents(c, db)
-	})
-	server.POST("/create_event", func(c *gin.Context) {
-		controllers.CreateEvent(c, db)
-	})
-	server.GET("/event/:id", func(c *gin.Context) {
-		controllers.GetEvent(c, db)
-	})
-	server.DELETE("/event/:id", func(c *gin.Context) {
-		controllers.DeleteEvent(c, db)
-	})
-	server.PUT("/event/:id", func(c *gin.Context) {
-		controllers.UpdateEvent(c, db)
-	})
 	server.POST("/signup", func(c *gin.Context) {
 		controllers.SignUp(c, db)
 	})
-
 	server.POST("/login", func(c *gin.Context) {
 		controllers.Login(c, db)
 	})
+
+	protected := server.Group("/api")
+	protected.Use(middleware.AuthMiddleware())
+
+	{
+		protected.GET("/events", func(c *gin.Context) {
+			controllers.GetEvents(c, db)
+		})
+		protected.POST("/create_event", func(c *gin.Context) {
+			controllers.CreateEvent(c, db)
+		})
+		protected.GET("/event/:id", func(c *gin.Context) {
+			controllers.GetEvent(c, db)
+		})
+		protected.DELETE("/event/:id", func(c *gin.Context) {
+			controllers.DeleteEvent(c, db)
+		})
+		protected.PUT("/event/:id", func(c *gin.Context) {
+			controllers.UpdateEvent(c, db)
+		})
+	}
 }
